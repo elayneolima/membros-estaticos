747ab18cd28ac67d2dd07ce5bd6b6578a7039f20
elayneolima <elayne04lima@gmail.com>
2025-04-23 00:38:40 -0300

    adicionando arquivos da aula de membros estaticos

---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization; 
+
+
+namespace MembrosEstaticos
+{
+    class Program
+
+    {
+        static double Pi = 3.14; 
+        static void Main(string[] Args)
+        {
+            
+            Console.WriteLine("entre com o vaor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio); 
+
+            Console.WriteLine("Circunfrência: " + circ.ToString("f2", CultureInfo.InvariantCulture));
+
+        }
+
+        static double Circunferencia(double raio)
+        {
+            return 2 * Pi * raio;
+        }
+    }
+}
+
+
+
+
